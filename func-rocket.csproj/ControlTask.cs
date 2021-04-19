9e02523c08920260960b34610f4501be5c54708d
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-04-19 20:36:46 +0500

    New

--- func-rocket.csproj/ControlTask.cs
+++ func-rocket.csproj/ControlTask.cs
@@ -4,9 +4,19 @@ namespace func_rocket
 {
 	public class ControlTask
 	{
-		public static Turn ControlRocket(Rocket rocket, Vector target)
+        public static double TotalAngle;
+        public static Turn ControlRocket(Rocket rocket, Vector target)
 		{
-			return Turn.None;
-		}
+            var distanceVector = target - rocket.Location;
+            var flagAngle = 0.0;
+            if (Math.Abs(distanceVector.Angle - rocket.Direction) < 0.5 ||
+                 Math.Abs(distanceVector.Angle - rocket.Velocity.Angle) < 0.5)
+                flagAngle = distanceVector.Angle - (rocket.Direction + rocket.Velocity.Angle)/2;
+            else
+                flagAngle = distanceVector.Angle - rocket.Direction;
+            if (flagAngle < 0)
+                return Turn.Left;
+            return flagAngle> 0 ? Turn.Right : Turn.None;
+        }
 	}
 }
