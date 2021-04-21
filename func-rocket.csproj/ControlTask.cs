39e8deca7d81c793b37efc77fe9132ac74d632d5
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-04-21 08:13:41 +0500

    New

--- func-rocket.csproj/ControlTask.cs
+++ func-rocket.csproj/ControlTask.cs
@@ -1,12 +1,28 @@
-using System;
-
+  using System;
+ 
 namespace func_rocket
 {
-	public class ControlTask
-	{
-		public static Turn ControlRocket(Rocket rocket, Vector target)
-		{
-			return Turn.None;
-		}
-	}
+    public class ControlTask
+    {
+        public static double TotalAngle;
+
+        public static Turn ControlRocket(Rocket rocket, Vector target)
+        {
+            TotalAngle = FindScalarMultiply(rocket.Velocity,target);
+            while(TotalAngle!=0)
+            {
+                if (TotalAngle < 0)
+                    return Turn.Right;
+                else if (TotalAngle > 0)
+                    return Turn.Left;
+                else
+                    return Turn.None;
+            }
+            return Turn.None;
+        }
+        public static double FindScalarMultiply(Vector a, Vector b)
+        {
+            return a.X * b.X + a.Y * b.Y / (Math.Sqrt(a.X * a.X + a.Y * a.Y) * Math.Sqrt(b.X * b.X + b.Y * b.Y));
+        }
+    }
 }
