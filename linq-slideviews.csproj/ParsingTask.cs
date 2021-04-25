7c4030fbc7110b1a823bcf56e72ab332061a7391
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-04-25 23:37:50 +0500

    New

--- linq-slideviews.csproj/ParsingTask.cs
+++ linq-slideviews.csproj/ParsingTask.cs
@@ -6,41 +6,49 @@ namespace linq_slideviews
 {
 	public class ParsingTask
 	{
-		/// <param name="lines">все строки файла, которые нужно распарсить. Первая строка заголовочная.</param>
-		/// <returns>Словарь: ключ — идентификатор слайда, значение — информация о слайде</returns>
-		/// <remarks>Метод должен пропускать некорректные строки, игнорируя их</remarks>
 		public static IDictionary<int, SlideRecord> ParseSlideRecords(IEnumerable<string> lines)
 		{
-			return lines
-				.Skip(1)
-				.Select(line => line.Split(';'))
-				.ToArray()
-				.Select(mbslide=>ToSlideRecord(mbslide))
-				.Where(slide => slide != null)
-				.ToDictionary(slide => slide.SlideId);
+			return lines.Skip(1)
+						.Select(line => line.Split(';'))
+						.Select(mbslide=>ToSlideRecord(mbslide))
+						.Where(slide => !(slide is null))
+						.ToDictionary(slide => slide.SlideId);
 		}
-		public static SlideRecord ToSlideRecord(string[] mbslide)
+
+		public static SlideRecord ToSlideRecord(string[] record)
 		{
 			int id;
 			SlideType slideType;
-			if (mbslide.Length == 3
-			&& mbslide[2] != string.Empty
-			&& int.TryParse(mbslide[0], out id)
-			&& Enum.TryParse(mbslide[1],true, out slideType))
-				return new SlideRecord(id, slideType, mbslide[2]);
+			if (record.Length == 3
+			&& record[2] != string.Empty
+			&& int.TryParse(record[0], out id)
+			&& Enum.TryParse(record[1],true, out slideType))
+				return new SlideRecord(id, slideType, record[2]);
 
 			return null;
         }
 
-		/// <param name="lines">все строки файла, которые нужно распарсить. Первая строка — заголовочная.</param>
-		/// <param name="slides">Словарь информации о слайдах по идентификатору слайда. 
-		/// Такой словарь можно получить методом ParseSlideRecords</param>
-		/// <returns>Список информации о посещениях</returns>
-		/// <exception cref="FormatException">Если среди строк есть некорректные</exception>
 		public static IEnumerable<VisitRecord> ParseVisitRecords(
 			IEnumerable<string> lines, IDictionary<int, SlideRecord> slides)
 		{
-			throw new NotImplementedException();
+			return lines.Skip(1)
+						.Select(s=> s.Split(';'))
+						.ToArray()
+						.Select(visit => ToVisitRecord(visit,slides));
 		}
+
+		public static VisitRecord ToVisitRecord(string[] record, IDictionary<int,SlideRecord> source)
+        {
+			int userid,
+				slideid;
+			DateTime visitDate;
+
+			if (int.TryParse(record[0], out userid)
+			&& int.TryParse(record[1], out slideid)
+			&& DateTime.TryParse(record[2]+" "+record[3], out visitDate)
+			&& source.ContainsKey(slideid))
+				return new VisitRecord(userid, slideid, visitDate, source[slideid].SlideType);
+			throw new FormatException($"Wrong line [{string.Join(";",record)}]");
+        }
 	}
 }
