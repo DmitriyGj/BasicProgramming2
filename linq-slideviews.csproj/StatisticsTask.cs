2b32077f968ef76ecc5ca28b9b512171af479b2a
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-04-30 07:26:48 +0500

    new

--- linq-slideviews.csproj/StatisticsTask.cs
+++ linq-slideviews.csproj/StatisticsTask.cs
@@ -8,7 +8,14 @@ namespace linq_slideviews
 	{
 		public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
 		{
-			throw new NotImplementedException();
+			var times = visits
+				.GroupBy(visit => visit.UserId)
+				.Select(g => g.OrderBy(x=>x.DateTime).Where(x=>x.SlideType ==slideType).Select(x=>x.DateTime.Minute).Where(x=>x>=1 && x<=120))
+				.SelectMany(g => g.Select(x => x));
+			var count = times.Count();
+			if (count != 0)
+				return count % 2 == 0 ?times.Average() : times.ToArray()[count / 2];
+			return 0.0;
 		}
 	}
 }
