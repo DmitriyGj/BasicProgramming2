6ebbdbf7d8c17c637c860eabccd5b2773634b748
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-05-14 23:26:15 +0500

    New

--- linq-slideviews.csproj/StatisticsTask.cs
+++ linq-slideviews.csproj/StatisticsTask.cs
@@ -8,14 +8,21 @@ namespace linq_slideviews
 	{
 		public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
 		{
-			var times = visits
+			try
+			{
+				return visits
 				.GroupBy(visit => visit.UserId)
-				.Select(g => g.OrderBy(x=>x.DateTime).Where(x=>x.SlideType ==slideType).Select(x=>x.DateTime.Minute).Where(x=>x>=1 && x<=120))
-				.SelectMany(g => g.Select(x => x));
-			var count = times.Count();
-			if (count != 0)
-				return count % 2 == 0 ?times.Average() : times.ToArray()[count / 2];
-			return 0.0;
+				.Select(group => group.OrderBy(item => item.DateTime)
+				.Bigrams())
+				.Select(bigram => bigram.Select(pair => pair)
+					.Where(pair => pair.Item1.SlideType == slideType).Distinct())
+				.SelectMany(bigram => bigram.Select(pair => (pair.Item2.DateTime - pair.Item1.DateTime).TotalMinutes)
+					.Where(time => time >= 1 && time <= 120)).Median();
+			}
+			catch
+			{
+				return 0.0;
+			}
 		}
 	}
 }
