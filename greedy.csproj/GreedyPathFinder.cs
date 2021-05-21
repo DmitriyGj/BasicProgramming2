fc4e2c7a67ec34cb66b88e0ed85f6fc74fd414f9
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-05-21 18:53:29 +0500

    New

--- greedy.csproj/GreedyPathFinder.cs
+++ greedy.csproj/GreedyPathFinder.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using Greedy.Architecture;
 using Greedy.Architecture.Drawing;
+using System.Linq;
 
 namespace Greedy
 {
@@ -9,7 +10,21 @@ namespace Greedy
 	{
 		public List<Point> FindPathToCompleteGoal(State state)
 		{
-			return new List<Point>();
+			var amountOfPickedChests = 0;
+			var finalPath = new PathWithCost(0);
+			if (state.Chests.Count < state.Goal)
+				return new List<Point>();
+			while (state.Goal > amountOfPickedChests )
+			{
+				var path = new DijkstraPathFinder().GetPathsByDijkstra(state, state.Position, state.Chests).FirstOrDefault();
+				if (path == null || path.Cost > state.Energy)
+				{ break; }
+				state.Position = path.End;
+				finalPath = new PathWithCost(path.Cost, finalPath.Path.Concat(path.Path.Skip(1)).ToArray());
+				state.Chests.Remove(state.Position);
+				amountOfPickedChests++;
+			}
+			return finalPath.Path;
 		}
 	}
 }
--- greedy.csproj/NotGreedyPathFinder.cs
+++ greedy.csproj/NotGreedyPathFinder.cs
@@ -2,14 +2,57 @@
 using System.Drawing;
 using Greedy.Architecture;
 using Greedy.Architecture.Drawing;
-
+using System.Linq;
+ 
 namespace Greedy
 {
+    class WayFinder
+    {
+        public LinkedList<Point> way { get; set; }
+        public int Price { get; set; }
+        public int Chests { get; set; }
+        public List<Point> notVisited { get; set; }
+        public WayFinder previous { get; set; }
+    }
+ 
 	public class NotGreedyPathFinder : IPathFinder
 	{
 		public List<Point> FindPathToCompleteGoal(State state)
 		{
-			return new List<Point>();
+            var pathFinder = new DijkstraPathFinder();
+            var ways = new Queue<WayFinder>();
+            ways.Enqueue(new WayFinder { way = new LinkedList<Point>(), Price = 0, Chests = 0, notVisited = state.Chests,previous=null});
+            WayFinder resultWay = null;
+            while(ways.Count != 0)
+            {
+                var findNext = false;
+                var currWay=ways.Dequeue();
+                var newWays = pathFinder.GetPathsByDijkstra(state, currWay.way.Count==0 ? state.Position: currWay.way.Last.Value,currWay.notVisited);
+                foreach(var e in newWays)
+                {
+                    var findWay=new WayFinder();
+                    findWay.Price = currWay.Price + e.Cost;
+                    if (findWay.Price > state.Energy)
+                        continue;
+                    findWay.way = new LinkedList<Point>(e.Path);
+                    findWay.way.RemoveFirst();
+                    findWay.previous = currWay;
+                    findWay.Chests = currWay.Chests + 1;
+                    findWay.notVisited = new List<Point>(currWay.notVisited);
+                    findWay.notVisited.Remove(findWay.way.Last.Value);
+                    ways.Enqueue(findWay);
+                    findNext = true;
+                }
+                if (!findNext && (resultWay==null || resultWay.Chests<currWay.Chests))
+                    resultWay=currWay;
+            }
+            var result = new List<Point>();
+            while(resultWay != null)
+            {
+                result.InsertRange(0, resultWay.way);
+                resultWay = resultWay.previous;
+            }
+            return result;
 		}
 	}
 }
