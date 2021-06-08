7b2feaa29776176c403211b17abef7178df7d6c5
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-06-09 00:05:48 +0500

    New

--- Tickets.csproj/TicketsTask.cs
+++ Tickets.csproj/TicketsTask.cs
@@ -13,21 +13,29 @@ namespace Tickets
         {
             if (totalSum % 2 != 0)
                 return 0;
-            var lengths = new Tuple<int, int>(halfLen + 1, totalSum / 2 + 1);
-            var opt = new BigInteger[lengths.Item1, lengths.Item2];
-            for (int i = 1; i != lengths.Item1; i++) 
-                opt[i, 0] = 1;
-            for(int i = 1; i != lengths.Item1; i++)
-                for(int j = 1; j != lengths.Item2; j++)
-                {
-                    if (j > i * 9) opt[i, j] = 0;
-                    else
-                    {
-                        opt[i, j] = opt[i, j - 1] + opt[i - 1, j];
-                        if (j > 9) opt[i, j] -= opt[i - 1, j - 10];
-                    }
-                }
-            return opt[halfLen, totalSum / 2]* opt[halfLen, totalSum / 2];
+            var opt = new BigInteger[halfLen+1, totalSum/2+1];
+            var result = CountOfTickets(opt, halfLen, totalSum/2);
+            return result*result;
+        }
+
+        static BigInteger CountOfTickets(BigInteger[,] tickets, int length, int sum)
+        {
+            if (sum==0)
+                return 1;
+            else if (length ==0)
+                return 0;
+            else if (tickets[length, sum] != 0)
+                return tickets[length, sum];
+            BigInteger localRes = 0;
+            for (int df = 0; df != 10; df++)
+            {
+                var diff = sum - df;
+                if (diff>-1)
+                    localRes += CountOfTickets(tickets, length - 1, diff);
+            }
+
+            tickets[length, sum] = localRes;
+            return localRes;
         }
     }
 }
