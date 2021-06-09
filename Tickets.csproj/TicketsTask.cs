4f26b2449be3df3d77b305e07eaa87308e965931
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-06-09 12:05:28 +0500

    New

--- Tickets.csproj/TicketsTask.cs
+++ Tickets.csproj/TicketsTask.cs
@@ -14,28 +14,28 @@ namespace Tickets
             if (totalSum % 2 != 0)
                 return 0;
             var opt = new BigInteger[halfLen+1, totalSum/2+1];
+            for(int i=0; i != halfLen+1;i++)
+                for(int j =0; j != totalSum/2+1;j++)
+                    opt[i, j] = -1;
             var result = CountOfTickets(opt, halfLen, totalSum/2);
             return result*result;
         }
 
         static BigInteger CountOfTickets(BigInteger[,] tickets, int length, int sum)
         {
-            if (sum==0)
+            if (sum == 0 && length != 0)
                 return 1;
-            else if (length ==0)
+            if (sum != 0 && length == 0) 
                 return 0;
-            else if (tickets[length, sum] != 0)
-                return tickets[length, sum];
-            BigInteger localRes = 0;
-            for (int df = 0; df != 10; df++)
+            if (tickets[length, sum] == -1)
             {
-                var diff = sum - df;
-                if (diff>-1)
-                    localRes += CountOfTickets(tickets, length - 1, diff);
+                BigInteger value = 0;
+                for (var df = 0; df < 10; df++)
+                    if (sum - df >= 0)
+                        value += CountOfTickets(tickets, length - 1, sum - df);
+                tickets[length, sum] = value;
             }
-
-            tickets[length, sum] = localRes;
-            return localRes;
+            return tickets[length,sum];
         }
     }
 }
