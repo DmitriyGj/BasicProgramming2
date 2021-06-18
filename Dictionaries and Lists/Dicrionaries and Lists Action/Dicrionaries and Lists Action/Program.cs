8e47ad764a56285818f2cb1c431e89869591aa0a
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-06-19 01:04:55 +0500

    New

---
+++ Dictionaries and Lists/Dicrionaries and Lists Action/Dicrionaries and Lists Action/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Dicrionaries_and_Lists_Action
+{
+    class Program
+    {
+        public static Dictionary<int, int> SequenceCollecotr = new Dictionary<int, int>(); 
+        static void Main()
+        {
+            DefaultSequence();
+            
+        }
+
+        public static void DefaultSequence()
+        {
+            int i = 1;
+            while (i != 50001)
+                SequenceCollecotr.Add(i,0);
+        }
+    }
+}
---
+++ Dictionaries and Lists/Dictionaries and Lists Action/Dictionaries and Lists Action/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Dictionaries_and_Lists_Action
+{
+    class Program
+    {
+        //75431
+        static void Main()
+        {
+            DefaultSequnce();
+            MySequnce();
+            Console.ReadKey();
+        }
+        public static void DefaultSequnce()
+        {
+            Dictionary<int, int> SequnceCollector = new Dictionary<int, int>();
+            var StopWatch = new Stopwatch();
+            StopWatch.Start();
+            int i = 0;
+            while (i != 50000)
+            {
+                SequnceCollector.Add(i, 0);
+                i++;
+            }
+            StopWatch.Stop();
+            Console.WriteLine($"Complited {StopWatch.ElapsedMilliseconds} - ms");
+        }
+        public static void MySequnce()
+        {
+            Dictionary<int, int> SequnceCollector = new Dictionary<int, int>();
+            var StopWatch = new Stopwatch();
+            StopWatch.Start();
+            int value = 1;
+            int bucketResizeCount = 1;
+            while(SequnceCollector.Keys.Count != 50000)
+            {
+                SequnceCollector[bucketResizeCount % 75431 ] = value;
+                value++;
+                if (value > bucketResizeCount) 
+                { 
+                    bucketResizeCount++; 
+                    value = 0; 
+                }
+            }
+            StopWatch.Stop();
+            Console.WriteLine($"Complited {StopWatch.ElapsedMilliseconds} - ms");
+        }
+    }
+}
