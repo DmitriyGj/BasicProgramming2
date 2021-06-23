765c0f5b2823ac21e3ffe5e46b33aa4cf0ab2aae
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2021-06-24 01:23:18 +0500

    д

--- BinaryTrees.csproj/Program.cs
+++ BinaryTrees.csproj/Program.cs
@@ -11,22 +11,8 @@ namespace BinaryTrees
     {
         static void Main(string[] args)
         {
-            //new AutoRun().Execute(args);
-            var a = new BinaryTree<int>();
-            
-                a.Add(6);
-            a.Add(2);
-            a.Add(3);
-            a.Add(4);
-            a.Add(8);
-            a.Add(9);
-            a.Add(1);
-            foreach (var nodes in a)
-            {
-                Console.WriteLine(nodes);
-            }
-            Console.WriteLine(a.Height);
-            Console.ReadKey();
+            new AutoRun().Execute(args);
+
         }
     }
 }
